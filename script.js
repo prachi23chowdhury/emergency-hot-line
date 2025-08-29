b27d0ceef332039a53fad73954814e8fff54b4b6
prachi23chowdhury <prachicmpi@gmail.com>
2025-08-29 17:10:04 +0600

    added my copy and alert section

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let count = 0;
 const historyData = [];
+let copyCount = 0;
+let balance =100;
+
 // heart count
 document.getElementById("heart-btn1").addEventListener("click", function(){
     heartCount('heart-btn1')
@@ -20,42 +23,94 @@ document.getElementById("heart-btn6").addEventListener("click", function(){
     heartCount('heart-btn6')
 })
 
+
+// coin section
+document.getElementById("call-btn1").addEventListener("click", function(){
+    cutCoin("call-btn1")
+   
+})
+document.getElementById("call-btn2").addEventListener("click", function(){
+    cutCoin("call-btn2")
+  
+})
+document.getElementById("call-btn3").addEventListener("click", function(){
+    cutCoin("call-btn3")
+   
+})
+document.getElementById("call-btn4").addEventListener("click", function(){
+    cutCoin("call-btn4")
+  
+})
+document.getElementById("call-btn5").addEventListener("click", function(){
+    cutCoin("call-btn5")
+
+})
+document.getElementById("call-btn6").addEventListener("click", function(){
+    cutCoin("call-btn6")
+   
+})
+
+
+// copy section
+
+document.getElementById("copy-btn1").addEventListener("click", function(){
+    copyCounts("copy-btn1")
+})
+document.getElementById("copy-btn2").addEventListener("click", function(){
+    copyCounts("copy-btn2")
+})
+document.getElementById("copy-btn3").addEventListener("click", function(){
+    copyCounts("copy-btn3")
+})
+document.getElementById("copy-btn4").addEventListener("click", function(){
+    copyCounts("copy-btn4")
+})
+document.getElementById("copy-btn5").addEventListener("click", function(){
+    copyCounts("copy-btn5")
+})
+document.getElementById("copy-btn6").addEventListener("click", function(){
+    copyCounts("copy-btn6")
+})
+
+
+// alert when click call
+const callButtons = document.getElementsByClassName('call')
+  for(const button of callButtons){
+    button.addEventListener('click', function(){
+      const index = this.dataset.index;
+      const p = document.getElementById('p' +index).innerText;
+      const num = document.getElementById('num' +index).innerText;
+
+      alert("📞" + p +" " + num)
+    })
+  }
+
+
+
 // function
 function heartCount(id){
     count++;
     document.getElementById('heart-count').innerText = count;
 }
 
-
-
-let balance = 100; 
-    
-    const balanceEl = document.getElementById("t-balance");
-    const callBtn = document.getElementById("call-btn");
-    const coinBtn = document.getElementById("coinBtn");
-    
-    
-    callBtn.addEventListener("click", function() {
-      if (balance >= 20) {
-        balance -= 20;
-        balanceEl.innerText = balance;
-        coinBtn.disabled = false; 
-        coinBtn.click(); 
-      }
-      
-       else {
-        if (balance < 20) {
-        alert("❌You don’t have enough coins. To make a call, you need at least 20 coins.");
-      } 
-      else {
-        alert("✅ Call was successful!");
-        coinBtn.disabled = true;
-      }
+function cutCoin(id) {
+    if (balance < 20) {
+      alert("❌ You don’t have enough coins. To make a call, you need at least 20 coins.");
+      return; // Stop the function if not enough balance
     }
-  });
-    
-    coinBtn.addEventListener("click", function() {
-    
-      console.log("Coin button clicked, balance is now " + balance);
-      coinBtn.disabled = true;
-    });
+
+    balance -= 20;
+    document.getElementById("t-balance").innerText = balance;
+  }
+function copyCounts(id){
+         copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+}
+
+
+
+  
+
+
+
+      
